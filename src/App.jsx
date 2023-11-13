2c12edb18a3f415ac8bdc0c904d272235991ecfb
abdelrhmaneladawy26 <abdelrhmaneladawy26@gmail.com>
2023-11-13 10:25:06 +0200

    Full Structure Components

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./App.css";
+import Login from "./AuthModule/Components/Login/Login";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/AuthModule/Components/ForgetPass/ForgetPass.jsx
@@ -0,0 +1,3 @@
+export default function ForgetPass() {
+  return <div>ForgetPass</div>;
+}
